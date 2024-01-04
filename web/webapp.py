be1be86baabbecf390568ae8f248897ca96e1d2c
Nguyễn Hoài Thanh Vĩ <67792385+devix2107@users.noreply.github.com>
2024-01-04 14:48:28 +0700

    Update webapp.py

--- web/webapp.py
+++ web/webapp.py
@@ -208,4 +208,4 @@ def handle_message(user: User, msg):
     asyncio.run(ipc_client.send(msg, user))
 
 if __name__ == '__main__':
-    socketio.run(app, host="127.0.0.1", port=5000)
+    socketio.run(app, host="0.0.0.0", port=5000)
